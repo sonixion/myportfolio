c091644a78e7d9ba1e64a85479101e0a5feef285
sixandsix <danielbilets@MacBook-Pro---sixisix.local>
2026-01-18 03:45:14 +0100

    feat: enable smooth scrolling

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
     // ВАЖЛИВО: Замініть '/REPO_NAME/' на назву вашого репозиторію на GitHub
     // Наприклад, якщо репозиторій називається 'my-portfolio', то пишіть '/my-portfolio/'
     // Якщо ви деплоїте на кореневий домен (username.github.io), залиште '/'
-    base: '/portfolio/',
+    base: '/myportfolio/',
 })
